abb68d7564c9663c5ad7e6c7c19ba508531119b0
kaeru <160379169+kaeru61@users.noreply.github.com>
2024-02-21 02:19:17 +0900

    mainpagemakecomponent

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,8 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
 import Header from '../components/Header'
+import MemoListItem from '../components/MemoListItem'
+import CircleButton from '../components/CircleButton'
 
 const Index = (): JSX.Element => {
   return (
@@ -9,42 +11,12 @@ const Index = (): JSX.Element => {
       <Header />
 
       <View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitile}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2024年2月20日 19:41</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitile}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2024年2月20日 19:41</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitile}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2024年2月20日 19:41</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
+        <MemoListItem />
+        <MemoListItem />
+        <MemoListItem />
       </View>
 
-      <View style={styles.circleButton}>
-        <Text style={styles.circleButtonLabel}>＋</Text>
-      </View>
+      <CircleButton />
 
     </View>
   )
@@ -54,46 +26,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff'
-  },
-  memoListItem: {
-    backgroundColor: '#ffffff',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingVertical: 16,
-    paddingHorizontal: 19,
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderColor: 'rgba(0,0,0,0.15)'
-  },
-  memoListItemTitile: {
-    fontSize: 16,
-    lineHeight: 32
-  },
-  memoListItemDate: {
-    fontSize: 12,
-    lineHeight: 16,
-    color: '#848484'
-  },
-  circleButton: {
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    backgroundColor: '#F09C4F',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    right: 40,
-    bottom: 40,
-    shadowColor: '#000000',
-    shadowOpacity: 0.25,
-    shadowRadius: 8,
-    shadowOffset: { width: 0, height: 8 },
-    elevation: 8
-  },
-  circleButtonLabel: {
-    color: '#ffffff',
-    fontSize: 40,
-    lineHeight: 48
   }
 })
 
